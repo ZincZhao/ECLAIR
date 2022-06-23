b99b3760e850e46e741b1bddc9e7edcc91d71e3a
Zinc Zhao <zinc.zhao@emory.edu>
2022-06-23 14:13:08 -0400

    Update Extraction Scripts.

---
+++ eclare/extraction/extract_script.py
@@ -0,0 +1,14 @@
+import json
+
+
+def extract(s: str) -> dict:
+    if not s.endswith('.json'):
+        return None
+    f = json.load(open(s))
+    f = f['ResumeParserData']
+    q = f['Qualification']
+    c = f['Certification']
+    e = f['Experience']
+    jp = f['JobProfile']
+    res = {'Qualification': q, 'Certification': c, 'Experience': e, 'JobProfile': jp}
+    return res
---
+++ tests/demo_extraction.py
@@ -0,0 +1,5 @@
+from eclare.extraction.extract_script import extract
+
+p = '../rchilli/40431.json'
+print(extract(p))
+print(type(extract(p)))
